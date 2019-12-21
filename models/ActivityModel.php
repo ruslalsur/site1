aef9b6aaa8e47345c3deaa6646f465ca2ae3e301
rusla <suhrugen@gmail.com>
2019-12-22 03:30:03 +0700

    изменение в новой ветке

--- models/ActivityModel.php
+++ models/ActivityModel.php
@@ -23,6 +23,8 @@ class ActivityModel extends Model
         1 => 'еженежельно'
     ];
 
+    //изменение в новой ветке
+
     public function beforeValidate()
     {
         $date = \DateTime::createFromFormat('d.m.Y', $this->deadline);
